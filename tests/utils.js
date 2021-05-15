96c2ae5237b7eed47278471cbf3b04b2b99385d4
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-05-16 00:17:17 +0300

    Chore: (tests) clearCache on module load is optional

--- tests/utils.js
+++ tests/utils.js
@@ -1,14 +1,14 @@
 import path from 'path';
 import { entry } from './constants';
 
-export function load(relPath) {
+export function load(relPath, clearCache) {
     const absPath = path.resolve(entry, relPath);
 
-    delete require.cache[require.resolve(absPath)];
+    if (clearCache) delete require.cache[require.resolve(absPath)];
     // eslint-disable-next-line security/detect-non-literal-require
     const result =  require(absPath);
 
-    delete require.cache[require.resolve(absPath)];
+    if (clearCache) delete require.cache[require.resolve(absPath)];
 
     return result;
 }
