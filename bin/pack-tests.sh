9d53bf30d295368e7f1e925c18b00e48cc16014e
Dmytro Pustovit <dmytro.pustovit@gmail.com>
2021-06-11 23:53:15 +0300

    Chore: integrate node-package-tester

--- bin/pack-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Packing ..."    
-TAR_NAME="$(npm pack 2>&1 | tail -1)"
-./bin/pack-tests.js $TAR_NAME
-mv $TAR_NAME tmp/package-tests/
--- bin/pack.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -rf tmp/package
-PACKAGE="$(npm pack 2>&1 | tail -1)"
-mkdir -p tmp
-tar -xvzf $PACKAGE -C tmp
-#mv $PACKAGE ${PACKAGE/-[0-9.]*.tgz/.tgz}
-echo "$PACKAGE"
